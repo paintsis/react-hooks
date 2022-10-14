beb18d8d9a4cfa27d4655efc0c9e99f9f6383cdf
Sherwin Gonzalez <paintsis@gmail.com>
2022-10-14 16:48:37 -0600

    class 127 reackt

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CounterApp } from './01-useState/CounterApp'
-import { CounterwithCustomHook } from './01-useState/CounterwithCustomHook'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+//import { CounterwithCustomHook } from './01-useState/CounterwithCustomHook'
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { HooksApp } from './HooksApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHook/>
   </React.StrictMode>
 )
